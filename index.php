01939b0b02574080360c3a39f536d0d2b7202807
kentucky-roberts <john@heels.com>
2015-06-20 17:22:42 -0400

    started adding baseline libs.

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+<meta charset="utf-8">
+<title>Palmer Motorsports - Account</title>
+{embed='global_embeds/.head-icons'}
+<meta author="Christopher J. Roberts">
+<meta description="Palmer Motorsports Your #1 Source for Motor Cycles, Dirt Bikes, UtVs, ATVs, SeaDoos, and Huskavarna Lawn and Garden Equipment and repair.">
+
+    <link rel="stylesheet" type="text/css" media="all" href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" media="all" href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+
+    <link href="{site_url}assets/css/style.css" rel="stylesheet">
+    <link href="{site_url}assets/classifieds-theme/css/owl.carousel.css" rel="stylesheet">
+    <link href="{site_url}assets/classifieds-theme/css/owl.theme.css" rel="stylesheet">
+    <link href="{site_url}assets/classifieds-theme/css/palmer-theme-styles.css" rel="stylesheet">
+    <style>
+    </style>
+    <style type="text/css">.cf-hidden { display: none; } .cf-invisible { visibility: hidden; }</style>
+    <script src="{site_url}assets/javascript/pace.min.js"></script>
+</head>
+<body>
+<div id="wrapper">
+    {embed='global_embeds/.top-nav-white'}
+
+
+
+    <div class="main-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3 page-sidebar">
+                    <aside>
+                        {embed='account/.page-sidebar'}
+                    </aside>
+                </div>
+                <div class="col-sm-9 page-content">
+                    
+                    {embed='account/.page-content'}
+              
+                </div>
+                
+                
+            </div><!--/.row-->
+        </div><!--/.container-->
+    </div><!--/.main-container-->
+
+            
+
+
+    {embed='global_embeds/.footer'}
+</div><!-- #wrapper -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.10.2.min.js"><\/script>')</script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+<script src="{site_url}assets/javascript/owl.carousel.min.js"></script>
+<script src="{site_url}assets/javascript/form-validation.js"></script>
+<script src="{site_url}assets/javascript/jquery.matchHeight-min.js"></script>
+<script src="{site_url}assets/javascript/hideMaxListItem.js"></script>
+<script src="{site_url}assets/javascript/jquery.fs.scroller.js"></script>
+<script src="{site_url}assets/javascript/jquery.fs.selecter.js"></script>
+<script src="{site_url}assets/javascript/jquery.parallax-1.1.js"></script>
+<script src="{site_url}assets/javascript/form-validation.js"></script>
+<script src="{site_url}assets/javascript/adsdetails.js"></script>
+<script src="{site_url}assets/javascript/jquery.scrollto.js"></script>
+<script src="{site_url}assets/javascript/script.js"></script>
+</body>
+</html>
+
